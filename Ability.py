3bc417ff58e7af1be32e26161859fda5395c9f7b
jmilis2000 <jahlil.milis97@gmail.com>
2019-10-19 20:58:25 -0400

    modified:   Ability.py

--- Ability.py
+++ Ability.py
@@ -1,7 +1,6 @@
 class Ability:
 
     # Class variables
-    #
     name = ""
     range = 0
     cell = 0
@@ -14,4 +13,25 @@ class Ability:
         self.cell = cell
         self.ap = ap
         self.dmg = dam
+
+    def Fireball(self):
+        self.name = 'Fireball'
+        self.range = 5
+        self.cell = 4
+        self.ap = 3
+        self.dmg = 1
+
+    def MagicArrow(self):
+        self.name = 'Magic Arrow'
+        self.range = 3
+        self.cell = 1
+        self.ap = 2
+        self.dmg = 2
+
+    def MeleeAttack(self):
+        self.name = 'Magic Slap'
+        self.range = 1
+        self.cell = 1
+        self.ap = 1
+        self.dmg = 1
 
