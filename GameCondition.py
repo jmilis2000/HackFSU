4737b4a622fb70eca04a431c51d022f8e36852cd
Vernicia Gage <53237130+Flostar15951@users.noreply.github.com>
2019-10-20 05:08:36 -0400

    Update GameCondition.py

--- GameCondition.py
+++ GameCondition.py
@@ -38,3 +38,64 @@ if Attack
         
     
 
+#From V: My code is below. Dont trust it, its busted.
+#Start
+#Load OldWitch
+#Load Young Witch
+if OldWitchHP == 0 and YoungWitchHP == 0:
+    print('You Both Lose')
+    elif OldWitchHP == 0 and YoungWitchHP != 0:
+        print('YoungWitch Wins')
+    elif OldWitchHP != 0 and YoungWitchHP ==0:
+        print('OldWitch Wins')
+    else continue:
+            if OldWitchAP ==0 and YoungWitchHP == 0:
+                end
+        elif OldWitchAP == 0 and YoungWitchAP != 0:
+            print('Waiting on YoungWitch')
+        elif OldwitchAP != 0 and YoungWitchAP == 0:
+            print('Waiting on OldWitch')
+        else continue:
+            #If Moving
+            if move:
+                location = #pull from character (x,y)
+                x0 = location()[0]
+                y0 = location()[1] #Z value, not y value
+                movelocation = #pull location from Unity
+                x = movelocation()[0]
+                y = movelocation()[1]
+                distance = sqrt((x^2)+(y^2))
+                    check distance <= AP #Need to figure out how to make it do this for both
+                        if false:
+                            print('Invalid Movement')
+                        else
+                            location(x,y) = (x0+x , y0+y)
+                end
+            if MagicArrow:
+                #Pull Magic Arrow stats
+                arrowto = #pull from character click
+                arrowfrom = location
+                arrowdistance = sqrt((x^2)+(y^2))
+                    check arrowdistance <= #Arrow Range
+                        if false:
+                            print('Invalid Target')
+                        else
+                    end #remember to do the damage calculation after all ap is used
+            if MeleeAttack:
+                meleefrom = location
+                melee to #pull from character click
+                melee distance = sqrt((x^2)+(y^2))
+                    check meleedistance<= #melee range
+                        if false:
+                            print('Invalid Target')
+                        else
+                    end #remember to do the damage calculation after all the ap is used
+            if FireBall:
+                firefrom = location
+                fireto = #pull from character click
+                firedistance = sqrt((x^2)+(y^2))
+                    check firedistance <= #fire range
+                        if false:
+                            print('Invalid Target')
+                        else
+                    end
